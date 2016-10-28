20aa66b567f211804727d83e117c3c230a00f629
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2016-10-28 13:51:31 +0200

    file descriptor leak fixed, removed log file check

--- src/main/java/dhus/listener/DhusListener.java
+++ src/main/java/dhus/listener/DhusListener.java
@@ -5,36 +5,29 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-// ssh dhus@91.134.230.97 'java -jar ~/dhus/dhus-listener.jar 3'
 public class DhusListener {
    private static void listenToFile(String fileName)
          throws IOException, InterruptedException {
+      BufferedReader reader = Files.newBufferedReader(
+            Paths.get(fileName), Charset.forName("UTF-8"));
       while(true) {
-         Path path = Paths.get(fileName);
-         if(Files.exists(path)) {
-            BufferedReader reader = 
-                  Files.newBufferedReader(Paths.get(fileName), Charset.forName("UTF-8"));
-            String line = reader.readLine();
-            if(line != null) {
-               if(line.contains("Server is ready...")) {
-                  // dhus is ready
-                  System.out.println("DHuS is ready!");
-                  reader.close();
-                  System.exit(0); // normal status returned (0)
-               }
-            } else {
-               // end of logs reached, close the reader
-               System.out.println("End of logs, resetting...");
+         String line = reader.readLine();
+         if(line != null) {
+            if(line.contains("Server is ready...")) {
+               // dhus is ready
+               System.out.println("DHuS is ready!");
                reader.close();
-               Thread.sleep(1000);
+               System.exit(0); // normal status returned (0)
             }
          } else {
-            // log file does not exist yet
-            System.out.println("Logs not found, checking again...");
+            // end of logs reached, close the reader, wait, then open a new one
+            System.out.println("End of logs, resetting...");
+            reader.close();
             Thread.sleep(1000);
+            reader = Files.newBufferedReader(
+                  Paths.get(fileName), Charset.forName("UTF-8"));
          }
       }
    }
