655eb6323bcd239416c4af52bd9043b54790f34f
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2016-10-31 11:16:49 +0100

    stop dhus at the end of start-listen.sh

--- start-listen.sh
+++ start-listen.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
-cd ~/dhus && ~/dhus/start.sh > /dev/null & java -jar ~/dhus-listener.jar 180 ~/dhus/dhus.log
+cd ~/dhus \
+&& ~/dhus/start.sh > /dev/null \
+& java -jar ~/dhus-listener.jar 180 ~/dhus/dhus.log \
+&& ~/dhus/stop.sh
