0db42b94c3e1007ad5cb8c3551527d94f0fc7d27
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2016-11-02 10:59:31 +0100

    added cp of start.sh in prepare.sh, removed stop.sh in start-listen.sh

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,6 @@ rm -rf ~/dhus/* \
 && unzip -o ~/dhus-software-distribution.zip -d ~/dhus \
 && rm ~/dhus-software-distribution.zip \
 && sed -ie "s|local_dhus|/home/dhus/dhus/local_dhus|" ~/dhus/etc/dhus.xml \
+&& cp ~/start.sh dhus/start.sh \
 && chmod +x ~/dhus/start.sh \
 && chmod +x ~/dhus/stop.sh
--- start-listen.sh
+++ start-listen.sh
@@ -2,4 +2,3 @@
 cd ~/dhus \
 && ~/dhus/start.sh > /dev/null 2>&1 \
 & java -jar ~/dhus-listener.jar 180 ~/dhus/dhus.log \
-&& ~/dhus/stop.sh
