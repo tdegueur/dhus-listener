72c424de0506ccbff8a0dadda1b29a3a6e631fab
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-09 10:36:17 +0100

    reverting

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 cd /home/dhus/go-dhus-environment \
-&& rm -rf /home/dhus/go-dhus-environment/dhus \
-&& mkdir /home/dhus/go-dhus-environment/dhus \
-&& unzip -o /home/dhus/go-dhus-environment/dhus-software-distribution.zip -d /home/dhus/go-dhus-environment/dhus \
-&& rm /home/dhus/go-dhus-environment/dhus-software-distribution.zip \
-&& sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" /home/dhus/go-dhus-environment/dhus/etc/dhus.xml \
-&& cp start.sh /home/dhus/go-dhus-environment/dhus/start.sh \
-&& chmod +x /home/dhus/go-dhus-environment/dhus/start.sh \
-&& chmod +x /home/dhus/go-dhus-environment/dhus/stop.sh \
-&& chmod +x /home/dhus/go-dhus-environment/start-listen.sh
+&& rm -rf ./dhus \
+&& mkdir ./dhus \
+&& unzip -o dhus-software-distribution.zip -d ./dhus \
+&& rm dhus-software-distribution.zip \
+&& sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
+&& cp start.sh ./dhus/start.sh \
+&& chmod +x ./dhus/start.sh \
+&& chmod +x ./dhus/stop.sh \
+&& chmod +x ./start-listen.sh
