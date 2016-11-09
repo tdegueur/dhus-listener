6ba1cd8ce9aed557ecf210246d3f47c688ea0100
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-09 09:54:52 +0100

    removed extra backslash in start-listen.sh

--- start-listen.sh
+++ start-listen.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd /home/dhus/go-dhus-environment/dhus \
 && ./start.sh > /dev/null 2>&1 \
-& java -jar /home/dhus/go-dhus-environment/dhus-listener.jar 180 dhus.log \
+& java -jar /home/dhus/go-dhus-environment/dhus-listener.jar 180 dhus.log
